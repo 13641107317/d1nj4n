9e5087a43dd605912f04fcef1c66c02e4dfb0ee9
wangpeng <wangpeng>
2018-05-17 11:08:49 +0800

    wp
    dialog style

---
+++ latte-core/src/main/java/com/flj/latte/ui/LatteLoader.java
@@ -0,0 +1,16 @@
+package com.flj.latte.ui;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatDialog;
+
+
+/**
+ * Created by wp on 2018/5/17.
+ */
+
+public class LatteLoader {
+    public static void showLoading(Context context, String type){
+        final AppCompatDialog dialog = new AppCompatDialog(context);
+
+    }
+}
