7fc6d6dc81649820f4ed4abad841160157c6093f
wangpeng <wangpeng>
2018-05-18 11:56:10 +0800

    wp
    注册界面

---
+++ latte-ec/src/main/java/com/flj/latte/ec/sign/SignUpDelegate.java
@@ -0,0 +1,25 @@
+package com.flj.latte.ec.sign;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.diabin.latte.ec.R;
+import com.flj.latte.delegates.LatteDelegate;
+
+/**
+ * Created by wp on 2018/5/18.
+ */
+
+public class SignUpDelegate extends LatteDelegate {
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_sign_up;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
+
+    }
+}
