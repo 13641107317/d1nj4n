07ff81b0d9e19baec54bb92307f2e2a48bddb0f0
wp <258188170@qq.com>
2018-05-24 21:45:12 +0800

    mac  loadwebView

--- latte-core/src/main/java/com/flj/latte/delegates/web/route/Router.java
+++ latte-core/src/main/java/com/flj/latte/delegates/web/route/Router.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.content.ContextCompat;
+import android.webkit.URLUtil;
+import android.webkit.WebView;
 
 import com.flj.latte.delegates.LatteDelegate;
 import com.flj.latte.delegates.web.WebDelegate;
@@ -31,15 +33,37 @@ public class Router {
             callPhone(delegate.getContext(), url);
             return true;
         }
-        final LatteDelegate parentDelegate = delegate.getParentDelegate();
+        final LatteDelegate topDelegate = delegate.getTopDelegate();
         final WebDelegateImpl webDelegate = WebDelegateImpl.create(url);
-        //如果没有父容器
-        if (parentDelegate == null) {
-            delegate.start(webDelegate);
-        } else {
-            parentDelegate.start(webDelegate);
-        }
+        topDelegate.start(webDelegate);
         return true;
+    }
+
+    private void loadWebPage(WebView webView, String url) {
+
+        if (webView != null) {
+            webView.loadUrl(url);
+        } else {
+            throw new NullPointerException("WebView is null;");
+        }
+
+    }
+
+    private void loadLocalPage(WebView webView, String url) {
+        loadWebPage(webView, "file:///android_asset/" + url);
+    }
+
+    private void loadPage(WebView webView, String url) {
+
+        if (URLUtil.isNetworkUrl(url) || URLUtil.isAssetUrl(url)) {
+            loadWebPage(webView, url);
+        } else {
+            loadLocalPage(webView, url);
+        }
+    }
+
+    public final void loadPage(WebDelegate delegate, String url) {
+        loadPage(delegate.getWebView(), url);
     }
 
     private void callPhone(Context context, String uri) {
